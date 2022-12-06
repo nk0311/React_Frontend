d3e0ab433d430db670af0b8e87be112b44df00bf
Navait Kumar <navaitk031@gmail.com>
2022-12-05 22:08:07 -0500

    source code for frontend repo

---
+++ src/components/containers/NewCourseContainer.js
@@ -0,0 +1,70 @@
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
+import NewCourseView from '../views/NewCourseView';
+import { addCourseThunk } from '../../store/thunks';
+
+
+class NewCourseContainer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          title: "", 
+          timeslot: "",
+          location: "", 
+          instructorId: null, 
+          redirect: false, 
+          redirectId: null
+        };
+    }
+
+    handleChange = event => {
+      this.setState({
+        [event.target.name]: event.target.value
+      });
+    }
+
+    handleSubmit = async event => {
+        event.preventDefault();
+        //dont need ID because the course has not been created yet
+        let course = {
+            title: this.state.title,
+            timeslot: this.state.timeslot,
+            location: this.state.location,
+            instructorId: this.state.instructorId
+        };
+        
+        let newCourse = await this.props.addCourse(course);
+
+        this.setState({
+          redirect: true, 
+          redirectId: newCourse.id
+        });
+    }
+
+    componentWillUnmount() {
+        this.setState({redirect: false, redirectId: null});
+    }
+
+    render() {
+      //go to single course view of newly created course
+        if(this.state.redirect) {
+          return (<Redirect to={`/course/${this.state.redirectId}`}/>)
+        }
+        return (
+          <NewCourseView 
+            handleChange={this.handleChange} 
+            handleSubmit={this.handleSubmit}      
+          />
+        );
+    }
+}
+
+const mapDispatch = (dispatch) => {
+    return({
+        addCourse: (course) => dispatch(addCourseThunk(course)),
+    })
+}
+
+export default connect(null, mapDispatch)(NewCourseContainer);
